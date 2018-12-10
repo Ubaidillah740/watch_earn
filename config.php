2fc35e5ee6d08bbbae3851bdaebe74d316fee2e7
Ubaidillah740 <45766000+Ubaidillah740@users.noreply.github.com>
2018-12-10 23:42:45 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-///Jangan hapus tanpa kutip nya(")
-///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
-///Ganti xxxxxxx dengan punya kalian
-///Capture dulu apk nya ambil yang di butuhkan
-$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh5mUsLqFaLW4f86wjYZrfmSvz4K2dpm0m3qakaPA0Iudg9260JKltM57yq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
-$meid = "1add232244f619fa";
-$os="android";
-$sign="fd6699e9e4043a1be4c5d1dbd0e13f5d4ab1915589a0bbf0ebf71f1a7478f7bd";
